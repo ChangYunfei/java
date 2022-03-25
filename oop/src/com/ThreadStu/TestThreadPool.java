de3e29545089abc1bef734593e6817dcbffd0ebb
changyunfei <1325822423@qq.com>
2022-03-25 12:06:18 +0800

    新增线程池

---
+++ oop/src/com/ThreadStu/TestThreadPool.java
@@ -0,0 +1,15 @@
+package com.ThreadStu;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class TestThreadPool {
+
+    public static void main(String[] args) {
+
+
+        ThreadPoolExecutor t=new ThreadPoolExecutor(3,3,100000000L,);
+
+
+
+    }
+}
